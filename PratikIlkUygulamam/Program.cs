f3dfca30a4f69343e7835f2c05a23777217e4a44
cang8 <0.gizemgunes@gmail.com>
2024-09-30 10:56:26 +0300

    Add project files.

---
+++ PratikIlkUygulamam/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Gizem Gunes \nPatika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim icin bu bootcampteyim!");
